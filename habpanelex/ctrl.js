4d00d4a65b81a054eff92afc581786900ecb26fb
Lucky Mallari <luckymallari@gmail.todotcom>
2019-01-23 19:47:34 -0800

    Moved html to a different html file Added modals Added seconds per panel
    Signed-off-by: Lucky Mallari <luckymallari@gmail.todotcom>

--- habpanelex/ctrl.js
+++ habpanelex/ctrl.js
@@ -2,13 +2,47 @@
     HPEx - (H)ab(P)anel(Ex)tension
     Extends HabPanel Functionality
     Lucky Mallari
+    https://github.com/LuckyMallari/hpex
+
+    MIT License
+
+    Copyright (c) 2019 Lucky Mallari https://github.com/LuckyMallari/hpex
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
 */
 (function () {
     'use strict';
     var app = angular.module('app', []);
 
-    app.service('HPExUtils', [
-        function () {
+    app.constant('DEFAULTCONFIG', {
+        "initComplete": true,
+        "isShowInDrawer": true,
+        "screensaver": {
+            "isEnabled": true,
+            "timeoutSeconds": 120,
+            "dashboardList": "",
+            "durationSeconds": 10
+        }
+    });
+    app.service('HPExUtils', ['DEFAULTCONFIG',
+        function (DEFAULTCONFIG) {
 
             var logTag = 'HPExUtils';
             var serviceApi = {};
@@ -17,20 +51,30 @@
 
             var synchrounousRestCall = function (method, itemName, body, mime) {
                 var xhr = new XMLHttpRequest();
-                xhr.open(method, '/rest/items/' + itemName, false);
+                var url = '/rest/items/' + itemName;
+                if ((method || "").toUpperCase() === 'GET') {
+                    url += '?lucky=' + Math.random().toString().substring(2, 10) + 'charms'
+                }
+                xhr.open(method, url, false);
                 mime = mime || "application/json";
                 if ((method || "").toUpperCase() !== "GET")
                     xhr.setRequestHeader("Content-Type", mime);
                 xhr.setRequestHeader("Accept", "application/json");
                 xhr.send(body && JSON.stringify(body) || null);
                 if (xhr.status !== 200 && xhr.status !== 201)
-                    return null;
-                return xhr.responseText;
+                    return {
+                        status: xhr.status,
+                        result: null
+                    };
+                return {
+                    status: xhr.status,
+                    result: xhr.responseText
+                };
             }
 
             serviceApi.getConfig = function (id) {
                 var c = synchrounousRestCall('GET', id);
-                if (c == null) {
+                if (c == null || c.status !== 200) {
                     // Config does not exist. Create an item for it.
                     log("Config item does not exist. Creating one..")
                     var data = {
@@ -43,18 +87,24 @@
                     // Then retrieve it
                     c = synchrounousRestCall('GET', id);
                 }
-                c = c && JSON.parse(c);
+                try {
+                    c = c && c.result && JSON.parse(c.result);
+                } catch {
+                    c = "{}"
+                }
                 c = c.state;
-                if (c && c === "NULL") {
+                try {
+                    c = JSON.parse(c);
+                } catch {
                     c = "{}";
                 }
-                c = JSON.parse(c);
                 if (c.initComplete) {
                     log("Config item found.")
                     return c;
                 }
 
                 log("Initializing Config.")
+                c = DEFAULTCONFIG;
                 c.initComplete = true;
                 synchrounousRestCall('POST', id, c, "text/plain");
                 log("Config init complete.")
@@ -73,16 +123,18 @@
 
             serviceApi.findParent = function (childSelector, parentSelector) {
                 var child = document.querySelector(childSelector);
+                var origParent = document.querySelector(parentSelector);
                 if (!child)
                     return false;
                 var parent = child.parentNode;
                 while (true) {
-                    if (parent.matches(parentSelector)) break;
-                    if (parent.matches(parentSelector)) break;
+                    if (parent == origParent)
+                        return parent;
+                    if (parent.tagName == "BODY") {
+                        return null;
+                    }
                     parent = parent.parentNode;
                 }
-                if (parent.matches('body')) parent = null;
-                return parent;
             };
 
             var init = function () { log("Init!"); return true; };
@@ -109,30 +161,37 @@
 
             serviceApi.saveConfig = function (newConfig) {
                 protectedConfigData = newConfig;
-                HPExUtils.saveConfig("habpanelExConfig", newConfig);
+                return HPExUtils.saveConfig("habpanelExConfig", newConfig);
             };
 
             serviceApi.getConfig = function () {
                 return angular.copy(protectedConfigData);
             }
 
+            serviceApi.toast = function (message) {
+
+            };
+
             var init = function () {
                 log("Init!");
                 return true;
             };
 
-            serviceApi.init = init();1
+            serviceApi.init = init(); 1
             return serviceApi;
         }
     ]);
 
-    app.service('HPExScrSaverService', ['HPExService', '$rootScope', 'HPExUtils', '$interval', 'PersistenceService',
-        function (HPExService, $rootScope, HPExUtils, $interval, PersistenceService) {
+    app.service('HPExScrSaverService', ['HPExService', '$rootScope', 'HPExUtils', '$interval', '$timeout', 'PersistenceService',
+        function (HPExService, $rootScope, HPExUtils, $interval, $timeout, PersistenceService) {
             var logTag = 'HPExScrSaverService';
             var log = function (s) { console.log(logTag + ": " + s); }
             var serviceApi = {};
             var eventsList = ["mousemove", "keyup", "keydown", "click"];
-            var timer = null;
+            var timers = {
+                idleTimer: null,
+                screenSaverTimer: null
+            }
             var config = {};
             var currentIndex = 0;
 
@@ -141,14 +200,17 @@
             }
 
             var resetTimer = function () {
-                stopTimer();
-                startTimer();
-                log("Idle reset.");
+                stopIdleTimer();
+                startIdleTimer();
             };
+
+            var idleHit = function() {
+                var interval = config.durationSeconds || 10;
+                timers.screenSaverTimer = $interval(next, interval * 1000);
+            }
 
             var next = function () {
                 var nextDb = config.dashboardList[currentIndex];
-                console.log('next: ' + nextDb);
                 HPExService.goToDashboard(nextDb);
                 if (currentIndex == config.dashboardList.length - 1)
                     currentIndex = 0;
@@ -156,24 +218,28 @@
                     currentIndex++;
             };
 
-            var startTimer = function () {
-                var timeout = config.timeoutSeconds || 10;
-                timer = $interval(next, timeout * 1000);
+            var startIdleTimer = function () {
+                var timeout = config.timeoutSeconds || 300;
+                timers.idleTimer = $timeout(idleHit, timeout * 1000);
             };
 
-            var stopTimer = function () {
-                $interval.cancel(timer);
-                timer = null;
+            var stopIdleTimer = function () {
+                $timeout.cancel(timers.idleTimer);
+                $interval.cancel(timers.screenSaverTimer);
+                timers.idleTime = null;
+                timers.screenSaverTimer = null;
             };
 
             var listen = function () {
-                for (var e of eventsList) {
+                for (var i = 0; i < eventsList.length; i++) {
+                    var e = eventsList[i];
                     document.addEventListener(e, resetTimer);
                 }
             };
 
             var unlisten = function () {
-                for (var e of eventsList) {
+                for (var i = 0; i < eventsList.length; i++) {
+                    var e = eventsList[i];
                     document.removeEventListener(e, resetTimer);
                 }
             };
@@ -187,8 +253,9 @@
                         return null;
                     }
                 }
-                
-                for (var d of a) {
+
+                for (var i = 0; i < a.length; i++) {
+                    var d = a[i];
                     d.id && d.id !== "HabPanelEx" && l.push(d.id);
                 }
                 return l;
@@ -196,7 +263,7 @@
 
             var initConfig = function () {
                 config = angular.copy(HPExService.getConfig());
-                config = config.screensaver;
+                config = config && config.screensaver || {};
                 if (!config.dashboardList) {
                     config.dashboardList = PersistenceService.getDashboards();
                 }
@@ -209,7 +276,7 @@
                     resetTimer();
                 } else {
                     unlisten();
-                    stopTimer();
+                    stopIdleTimer();
                 }
             };
 
@@ -258,11 +325,35 @@
         }]
     );
 
+    app.service('HPExModalService', ['$uibModal',
+        function ($uibModal) {
+            var serviceApi = {};
+
+            var bucket = { modalInstance: null };
+            serviceApi.open = function (title, message) {
+                bucket.modalInstance = $uibModal.open({
+                    templateUrl: '/static/habpanelex/tpl/modal.html',
+                    backdrop: 'static',
+                    controller: function ($scope, $uibModalInstance) {
+                        $scope.close = $uibModalInstance.close;
+                        $scope.title = title;
+                        $scope.message = message;
+                    }
+                })
+            };
+            serviceApi.close = function () {
+                bucket.modalInstance.close();
+            };
+            return serviceApi;
+        }
+    ]);
+
+
     /*
         Main Settings Controller
     */
-    app.controller('HPExCtrl', ['$scope', 'HPExService', 'HPExUtils', '$rootScope', 'HPExScrSaverService',
-        function ($scope, HPExService, HPExUtils, $rootScope, HPExScrSaverService) {
+    app.controller('HPExCtrl', ['$scope', 'HPExService', 'HPExUtils', '$rootScope', 'HPExScrSaverService', 'HPExModalService',
+        function ($scope, HPExService, HPExUtils, $rootScope, HPExScrSaverService, HPExModalService) {
             var logTag = 'HPExCtrl';
             $scope.header = "HPEx";
 
@@ -276,12 +367,16 @@
             };
 
             $scope.saveConfig = function () {
-                HPExService.saveConfig($scope.config);
-                $rootScope.$broadcast('HPExEvent.configChanged');
+                var r = HPExService.saveConfig($scope.config);
+                if (r.status === 201)
+                    $rootScope.$broadcast('HPExEvent.configChanged');
+                else
+                    HPExModalService.open('Config', 'Saved');
             }
 
             $scope.cancelConfig = function () {
                 onInit();
+                HPExModalService.open('Config', 'Canceled');
             }
 
             var log = function (s) { console.log(logTag + ": " + s); }
@@ -297,6 +392,14 @@
 
             $scope.$on('$destroy', onDestroy);
             onInit();
-        }]
-    );
+        }
+    ]);
+
+    app.directive('habPanelEx', function () {
+        return {
+            templateUrl: '/static/habpanelex/tpl/main.html',
+            restrict: 'A',
+            controller: 'HPExCtrl'
+        };
+    });
 })();
